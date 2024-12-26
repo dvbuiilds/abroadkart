a7232a9a480954704c89ce516721843ab7d9a758
dvbuiilds <varshneydhairya4@gmail.com>
2024-12-26 16:21:08 +0530

    chore: added Navbar to _app.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import "@app/styles/globals.css";
+// THIRD PARTY
 import type { AppProps } from "next/app";
 
+// COMPONENTS
+import { Navbar } from "@app/components/Navbar";
+
+// STYLES
+import "@app/styles/globals.css";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
