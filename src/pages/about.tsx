8e12ad5ef7bd8ce4eb420e4eb796a1892efb78c2
dvbuiilds <varshneydhairya4@gmail.com>
2024-12-26 16:21:48 +0530

    chore: added about page.

---
+++ src/pages/about.tsx
@@ -0,0 +1,13 @@
+const About: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold">About AbroadKart</h1>
+      <p className="mt-4 text-lg text-gray-700">
+        Learn more about our mission to connect people with global
+        opportunities.
+      </p>
+    </div>
+  );
+};
+
+export default About;
