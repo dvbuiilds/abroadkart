879412b4e0ae6cdc98131dac471174e837cc0132
dvbuiilds <varshneydhairya4@gmail.com>
2024-12-31 00:07:10 +0530

    chore: added new route on app /dashboard.

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold">Dashboard</h1>
+      <p className="mt-4 text-lg text-gray-700">
+        Here you will get all of your support.
+      </p>
+    </div>
+  );
+};
+
+export default Dashboard;
