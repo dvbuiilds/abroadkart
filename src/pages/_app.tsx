ce9407543af3b3dfd952e0097bd7f74237db9615
dvbuiilds <varshneydhairya4@gmail.com>
2024-12-31 00:08:13 +0530

    chore: adding UserDataProvider as wrapper.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,12 +6,13 @@ import { Navbar } from "@app/components/Navbar";
 
 // STYLES
 import "@app/styles/globals.css";
+import { UserDataProvider } from "@app/context/UserDataContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <UserDataProvider>
       <Navbar />
       <Component {...pageProps} />
-    </>
+    </UserDataProvider>
   );
 }
