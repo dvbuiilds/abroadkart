a8377b13ed424a4e90fdd34e825aec4a3073539b
dvbuiilds <varshneydhairya4@gmail.com>
2024-12-31 16:17:00 +0530

    chore: added api paths & endpoints.

---
+++ src/config/api-config.ts
@@ -0,0 +1,11 @@
+export const apiPaths = {
+  development: "http://localhost:3000",
+  production: "https://my-production-api.com",
+};
+
+export const apiEndPoints = {
+  login: "/api/auth/login",
+  signup: "/api/auth/signup",
+  session: "/api/auth/session",
+  logout: "/api/auth/logout",
+};
