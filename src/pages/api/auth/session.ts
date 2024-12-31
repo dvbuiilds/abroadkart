4797de15b08ce12f1867e0c387460ca726f2fe6c
dvbuiilds <varshneydhairya4@gmail.com>
2024-12-31 16:12:05 +0530

    chore(api/auth/session): structured the return responses of API.

--- src/pages/api/auth/session.ts
+++ src/pages/api/auth/session.ts
@@ -1,14 +1,22 @@
 import { NextApiRequest, NextApiResponse } from "next";
+
+// TYPES
+import { ResponseType, User } from "@app/types/api-types";
+
+// UTILS
 import mongoDBClient from "../../../server/db/mongodb"; // Import MongoDB client
 
 export default async function getSessionHandler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse<ResponseType<User>>
 ) {
   const { sessionID } = req.cookies;
 
   if (!sessionID) {
-    return res.status(401).json({ error: "Not authenticated" });
+    return res.status(401).json({
+      success: false,
+      error: { message: "Not authenticated", status: 401 },
+    });
   }
 
   // Get the database instance
@@ -20,19 +28,26 @@ export default async function getSessionHandler(
     .findOne({ sessionId: sessionID });
 
   if (!session || new Date() > new Date(session.expiresAt)) {
-    return res.status(401).json({ error: "Session expired" });
+    return res.status(401).json({
+      success: false,
+      error: { message: "Session expired", status: 401 },
+    });
   }
 
   // Retrieve user details
   const user = await db.collection("users").findOne({ _id: session.userId });
 
   if (!user) {
-    return res.status(404).json({ error: "User not found" });
+    return res.status(404).json({
+      success: false,
+      error: { message: "User not found", status: 404 },
+    });
   }
 
   return res.status(200).json({
-    user: {
-      id: user._id,
+    success: true,
+    data: {
+      id: user._id.toString(),
       name: user.name,
       email: user.email,
       phoneNumber: user.phoneNumber,
