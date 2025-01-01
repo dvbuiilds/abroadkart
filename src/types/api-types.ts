4cd01a732f90791404a23162da824ce715fa82cb
dvbuiilds <varshneydhairya4@gmail.com>
2025-01-01 22:24:18 +0530

    chore: added user data type for next auth.

--- src/types/api-types.ts
+++ src/types/api-types.ts
@@ -5,6 +5,17 @@ export type User = {
   phoneNumber: string;
 };
 
+export interface UserSignedInWithAuthProvider {
+  id: string; // Primary key (your internal ID)
+  googleId: string; // From "sub" field
+  email: string;
+  emailVerified: boolean;
+  name: string;
+  picture?: string;
+  provider: string; // e.g., "google"
+  phoneNumber?: string;
+}
+
 export type ResponseType<T> =
   | {
       success: true;
