5b4a8117ab51f40dce464a9b609e27cadf80a984
dvbuiilds <varshneydhairya4@gmail.com>
2025-01-04 10:27:30 +0530

    chore: adding SessionProvider to _app.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,14 +7,17 @@ import { Navbar } from "@app/components/Navbar";
 // STYLES
 import "@app/styles/globals.css";
 import { UserSessionProvider } from "@app/context/UserSessionContext";
+import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <UserSessionProvider>
-      <div className="h-screen flex flex-col overflow-y-auto">
-        <Navbar />
-        <Component {...pageProps} />
-      </div>
-    </UserSessionProvider>
+    <SessionProvider session={pageProps.session}>
+      <UserSessionProvider>
+        <div className="h-screen flex flex-col overflow-y-auto">
+          <Navbar />
+          <Component {...pageProps} />
+        </div>
+      </UserSessionProvider>
+    </SessionProvider>
   );
 }
