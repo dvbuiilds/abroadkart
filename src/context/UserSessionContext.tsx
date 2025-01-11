41c605ba5576341fca584f544e7c3510b2c17a4e
dvbuiilds <varshneydhairya4@gmail.com>
2025-01-11 17:35:04 +0530

    chore: modified the provider for triggerLogin, logout, session status & data using next-auth.

--- src/context/UserSessionContext.tsx
+++ src/context/UserSessionContext.tsx
@@ -1,25 +1,22 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import { apiEndPoints, apiPaths } from "@app/config/api-config";
-import type { ResponseType, User } from "../types/api-types";
+import type { Session } from "next-auth";
 import { useSession, signIn, signOut } from "next-auth/react";
 
-type UserDataFetchStatusType =
-  | "idle"
-  | "loading"
-  | "success"
-  | "failure"
-  | "error";
-
-type UserSessionStatus = "idle" | "loggedIn" | "loggedOut" | "anonymous";
+import type { ResponseType, SessionProvider, User } from "../types/api-types";
+import { apiEndPoints, apiPaths } from "@app/config/api-config";
 
 interface UserSessionContextType {
   user: User | null;
-  userSessionStatus: UserSessionStatus;
+  sessionProvider: SessionProvider;
+  activeSession: {
+    status: "authenticated" | "loading" | "unauthenticated";
+    data: Session | null;
+  };
   triggerLogin: (
     arg:
       | {
-          provider: "manual";
+          provider: "email";
           formData: {
             email: string;
             password: string;
@@ -30,125 +27,26 @@ interface UserSessionContextType {
         }
   ) => Promise<void>;
   triggerLogout: () => void;
-  triggerFetchSession: () => Promise<void>;
 }
 
 const UserSessionContext = createContext<UserSessionContextType | undefined>(
   undefined
 );
 
-const environment: "development" | "production" =
-  (process.env.ENVIRONMENT as "development" | "production") ?? "development";
-
-// Right now not being used. But can be used to make API calls.
-const userLoginUrl = `${apiPaths[environment]}${apiEndPoints.login}`;
-const userLogoutUrl = `${apiPaths[environment]}${apiEndPoints.logout}`;
-const userSessionUrl = `${apiPaths[environment]}${apiEndPoints.session}`;
-
-const handleUserLoginByEmail = async (
-  formData: { email: string; password: string },
-  onSuccessCallback: (userData: User) => void,
-  onFailureCallback: (
-    errorMessage: string,
-    status: UserDataFetchStatusType
-  ) => void
-) => {
-  try {
-    const response = await fetch(apiEndPoints.login, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    });
-
-    const jsonResponse: ResponseType<User> = await response.json();
-    if (jsonResponse.success) {
-      onSuccessCallback(jsonResponse.data);
-    } else {
-      onFailureCallback(jsonResponse.error.message, "failure");
-    }
-  } catch (error) {
-    console.error("Failed to login:", error);
-    onFailureCallback(
-      "Internal Server Error. Please try again after some time.",
-      "error"
-    );
-  }
-};
-
-const handleUserLoginByAuthProvider = async (provider: string) => {
-  console.log("Inside handleUserLoginByAuthProvider.");
-  // debugger;
-  const response = await signIn(provider, {
-    redirect: false,
-    callbackUrl: "/dashboard",
-  });
-  // debugger;
-  console.log(
-    "@dhairya handleUserLoginByAuthProvider response: ",
-    JSON.stringify(response, null, 2)
-  );
-};
-
-const handleLogout = async (onLogoutCallback: () => void) => {
-  try {
-    const response = await fetch(apiEndPoints.logout, {
-      method: "POST",
-      credentials: "include", // Ensures cookies are sent with the request
-    });
-
-    onLogoutCallback(); // Whatever the response is, we are logging out the user.
-
-    // const jsonResponse: ResponseType<User> = await response.json();
-    // if (jsonResponse.success) {
-    //   onLogoutCallback();
-    // } else {
-    //   // something i need to plan like clearing all cookies and then calling the callback.
-    // }
-  } catch (error) {
-    console.error("Error during logout:", error);
-    alert("Failed to logout. Please try again.");
-  }
-};
-
-const fetchActiveSession = async (
-  sessionFoundCallback: (userData: User) => void,
-  sessionNotFoundCallback: () => void
-) => {
-  try {
-    const response = await fetch(apiEndPoints.session);
-    const jsonResponse: ResponseType<User> = await response.json();
-
-    if (jsonResponse.success) {
-      sessionFoundCallback(jsonResponse.data);
-    } else {
-      sessionNotFoundCallback();
-    }
-  } catch (error) {
-    console.error("Failed to fetch session:", error);
-    sessionNotFoundCallback();
-  }
-};
-
-export const UserSessionProvider: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => {
-  const [user, updateUser] = useState<User | null>(null);
-  const [userSessionStatus, updateUserSessionStatus] =
-    useState<UserSessionStatus>("anonymous");
-
+export const UserSessionProvider: React.FC<{
+  children: React.ReactNode;
+}> = ({ children }) => {
   const router = useRouter();
-  const { data, status } = useSession();
-  console.log("@Dhairya inside UserSessionProvider checking useSession: ", {
-    data,
-    status,
-  });
+  const activeSession = useSession();
+
+  const [user, updateUser] = useState<User | null>(null);
+  const [sessionProvider, updateSessionProvider] =
+    useState<SessionProvider>("no-provider");
 
   const triggerLogin = async (
     arg:
       | {
-          provider: "manual";
+          provider: "email";
           formData: {
             email: string;
             password: string;
@@ -156,73 +54,63 @@ export const UserSessionProvider: React.FC<{ children: React.ReactNode }> = ({
         }
       | { provider: "google" }
   ) => {
-    if (arg.provider === "manual") {
-      console.log('Inside triggerLogin. arg.provider === "manual"');
-      // the fetchStatus is also to be edited here.
-      handleUserLoginByEmail(
-        arg.formData,
-        (userData) => {
-          updateUser(userData);
-          updateUserSessionStatus("loggedIn");
-        },
-        (errorMessage, status) => {
-          updateUser(null);
-          updateUserSessionStatus("anonymous");
-          alert(errorMessage);
-        }
-      );
-    } else {
-      console.log('Inside triggerLogin. arg.provider === "google"');
-      handleUserLoginByAuthProvider(arg.provider);
+    switch (arg.provider) {
+      case "email": {
+        signIn("credentials", arg.formData);
+        router.push("/dashboard");
+        break;
+      }
+      default: {
+        signIn(arg.provider);
+        break;
+      }
     }
   };
 
   const triggerLogout = () => {
-    handleLogout(() => {
-      updateUser(null);
-      updateUserSessionStatus("loggedOut");
-    });
+    signOut();
+    router.push("/login");
+    updateSessionProvider("no-provider");
+    updateUser(null);
   };
 
-  const triggerFetchSession = async () => {
-    console.log("Inside triggerFetchSession.");
-    fetchActiveSession(
-      (userData) => {
-        updateUser(userData);
-        updateUserSessionStatus("loggedIn");
-      },
-      () => {
-        updateUser(null);
-        updateUserSessionStatus("anonymous");
-      }
+  const fetchUserDetails = async () => {
+    const response = await fetch(
+      `${apiPaths.development}${
+        apiEndPoints.getUser
+      }?email=${encodeURIComponent(activeSession.data?.user?.email)}`
     );
+    const jsonResponse: ResponseType<User> = await response.json();
+    if (jsonResponse.success) {
+      updateUser(jsonResponse.data);
+      updateSessionProvider(jsonResponse.data.provider ?? "credentials");
+      console.log("@Dhairya the fetched details are: ", jsonResponse);
+    } else {
+      updateUser(null);
+      updateSessionProvider("no-provider");
+    }
   };
 
   useEffect(() => {
-    console.log("Inside UserSessionContext useEffect.");
-    // Fetch user session if userSessionStatus is not "loggedIn".
-    if (userSessionStatus !== "loggedIn") {
-      triggerFetchSession();
-    }
-
-    if (userSessionStatus === "loggedIn" && router.pathname === "/login") {
-      router.push("/dashboard"); // Redirect logged-in users to /dashboard
+    if (activeSession.status === "authenticated") {
+      fetchUserDetails();
+      if (router.pathname === "/login") router.push("/dashboard");
     } else if (
-      userSessionStatus !== "loggedIn" &&
+      activeSession.status === "unauthenticated" &&
       router.pathname === "/dashboard"
     ) {
-      router.push("/login"); // Redirect logged-out users to /login
+      router.push("/login");
     }
-  }, [userSessionStatus]);
+  }, [activeSession.status, router.pathname]);
 
   return (
     <UserSessionContext.Provider
       value={{
         user,
-        userSessionStatus,
+        sessionProvider,
+        activeSession,
         triggerLogin,
         triggerLogout,
-        triggerFetchSession,
       }}
     >
       {children}
