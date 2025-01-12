0e7bc066dbff6353c263869620c5fd6067a89893
dvbuiilds <varshneydhairya4@gmail.com>
2025-01-12 14:37:20 +0530

    chore: handling error messages + added google icon.

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,45 +1,21 @@
 import { useState } from "react";
+import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
+
+import GoogleLogo from "../../public/google-logo-64.png";
+
 import { useUserSession } from "@app/context/UserSessionContext";
 
-const GoogleIcon = () => (
-  <svg
-    className="w-6 h- me-2"
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 32 32"
-    data-name="Layer 1"
-    id="Layer_1"
-  >
-    <path
-      d="M23.75,16A7.7446,7.7446,0,0,1,8.7177,18.6259L4.2849,22.1721A13.244,13.244,0,0,0,29.25,16"
-      fill="#00ac47"
-    />
-    <path
-      d="M23.75,16a7.7387,7.7387,0,0,1-3.2516,6.2987l4.3824,3.5059A13.2042,13.2042,0,0,0,29.25,16"
-      fill="#4285f4"
-    />
-    <path
-      d="M8.25,16a7.698,7.698,0,0,1,.4677-2.6259L4.2849,9.8279a13.177,13.177,0,0,0,0,12.3442l4.4328-3.5462A7.698,7.698,0,0,1,8.25,16Z"
-      fill="#ffba00"
-    />
-    <polygon
-      fill="#2ab2db"
-      points="8.718 13.374 8.718 13.374 8.718 13.374 8.718 13.374"
-    />
-    <path
-      d="M16,8.25a7.699,7.699,0,0,1,4.558,1.4958l4.06-3.7893A13.2152,13.2152,0,0,0,4.2849,9.8279l4.4328,3.5462A7.756,7.756,0,0,1,16,8.25Z"
-      fill="#ea4435"
-    />
-    <polygon
-      fill="#2ab2db"
-      points="8.718 18.626 8.718 18.626 8.718 18.626 8.718 18.626"
-    />
-    <path
-      d="M29.25,15v1L27,19.5H16.5V14H28.25A1,1,0,0,1,29.25,15Z"
-      fill="#4285f4"
-    />
-  </svg>
-);
+const errorMessages: { [key: string]: string } = {
+  CredentialsSignin: "Invalid email or password. Please try again.",
+  OAuthSignin: "Error signing in with OAuth.",
+  OAuthCallback: "Error during OAuth callback.",
+  OAuthCreateAccount: "Could not create OAuth account.",
+  EmailCreateAccount: "Could not create email account.",
+  Callback: "Error during sign-in callback.",
+  Default: "An unknown error occurred.",
+};
 
 const Login = () => {
   const [formData, setFormData] = useState<{
@@ -49,6 +25,12 @@ const Login = () => {
     email: "",
     password: "",
   });
+  const router = useRouter();
+  const { error } = router.query;
+
+  const errorMessage = error
+    ? errorMessages[error as string] || errorMessages.Default
+    : null;
 
   const disableSubmitButton = Object.entries(formData).reduce((acc, curr) => {
     return acc || !Boolean(curr[1].length);
@@ -77,12 +59,19 @@ const Login = () => {
         aria-disabled={disableSubmitButton}
       >
         <h1 className="text-3xl font-bold mb-4 text-center">Login</h1>
+        {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
         <button
           type="button"
           className="self-center border border-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 mb-2 hover:bg-gray-50"
           onClick={() => triggerLogin({ provider: "google" })}
         >
-          <GoogleIcon />
+          <Image
+            src={GoogleLogo}
+            alt="Google SignIn Icon"
+            width={20}
+            height={20}
+            className="mr-2"
+          />
           Sign in with Google
         </button>
         <div className="flex items-center justify-center gap-4 py-2">
