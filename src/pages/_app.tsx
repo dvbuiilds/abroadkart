6b46ec289679a43e363b98ad18ab7cdd495c0d9f
dvbuiilds <varshneydhairya4@gmail.com>
2025-01-12 20:42:17 +0530

    chore: added getLayout method for selective layouts of specific routes like /dashboard + shifted the component of dashboard to /dashboard/index.tsx.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,36 @@
 // THIRD PARTY
 import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import { useRouter } from "next/router";
 
 // COMPONENTS
+import { DashboardLayout } from "@app/components/DashboardLayout";
 import { Navbar } from "@app/components/Navbar";
+import { UserSessionProvider } from "@app/context/UserSessionContext";
 
 // STYLES
 import "@app/styles/globals.css";
-import { UserSessionProvider } from "@app/context/UserSessionContext";
-import { SessionProvider } from "next-auth/react";
+
+const getLayoutFromPathName = (pathName: string, children: React.ReactNode) => {
+  if (pathName.startsWith("/dashboard")) {
+    return <DashboardLayout>{children}</DashboardLayout>;
+  }
+  return children;
+};
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const ComponentWithLayout = getLayoutFromPathName(
+    router.pathname,
+    <Component {...pageProps} />
+  );
+
   return (
     <SessionProvider session={pageProps.session}>
       <UserSessionProvider>
         <div className="h-screen flex flex-col overflow-y-auto">
           <Navbar />
-          <Component {...pageProps} />
+          {ComponentWithLayout}
         </div>
       </UserSessionProvider>
     </SessionProvider>
