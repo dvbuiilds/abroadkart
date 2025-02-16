eb9c3c9ab9f7dba8bb55605845b2834e5577aaef
dvbuiilds <varshneydhairya4@gmail.com>
2025-02-16 17:41:55 +0530

    chore: added form submit api handling.

--- src/pages/dashboard/pre-counselling-form.tsx
+++ src/pages/dashboard/pre-counselling-form.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 
+// COMPONENTS
 import { FormProgressBar } from "@app/components/FormProgressBar/FormProgressBar";
 import { Form } from "@app/components/Form";
 
+// TYPES
 import type { Questionnaire } from "@app/types/form-types";
 
+// UTILS
 import formData from "@app/utils/data/pre-counselling-form-questions.json";
+import { useUserSession } from "@app/context/UserSessionContext";
+
+// CONFIGS
+import { apiEndPoints, apiPaths } from "@app/config/api-config";
+import { useRouter } from "next/router";
 
 const totalSteps = formData.sets.length;
 const LOCAL_STORAGE_TIME = 1000 * 60;
@@ -27,15 +35,52 @@ const makeQuestionAnswersObject = () => {
 
 const formNames = formData.sets.map((set) => set.name);
 
+const handleFormSubmitAPICall = async (data: Questionnaire, email: string) => {
+  console.log("@@ Submitting the form...", data);
+  const response = await fetch(
+    `${apiPaths.development}${apiEndPoints.preCounsellingForm}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        email,
+      },
+      body: JSON.stringify(data),
+    }
+  );
+  const responseData = await response.json();
+  console.log("@@ FORM SUBMIT response: ", responseData);
+  return responseData;
+};
+
 export default function PreCounsellingForm() {
+  const { user, fetchUserDetails } = useUserSession();
   const [formData, updateFormData] = React.useState<Questionnaire>(
     makeQuestionAnswersObject()
   );
-  const [currentStep, updateCurrentStep] = React.useState<number>(0);
+  const [currentStep, updateCurrentStep] = React.useState<number>(4);
+  const [apiStatus, updateAPIStatus] = React.useState<
+    "idle" | "loading" | "success" | "error"
+  >("idle");
+  const navigation = useRouter();
 
+  // const isEveryQuestionAnswered = true;
   const isEveryQuestionAnswered = formData[currentStep].info.every(
     (info) => info.answer.length >= 5
   );
+
+  const handleFormSubmit = async () => {
+    updateAPIStatus("loading");
+    const response = await handleFormSubmitAPICall(formData, user.email);
+    if (response.success) {
+      updateAPIStatus("success");
+      fetchUserDetails();
+      navigation.push("/dashboard");
+    } else {
+      updateAPIStatus("error");
+      alert("Error in submitting the form. Please try again later.");
+    }
+  };
 
   const nextStep = () => {
     if (!isEveryQuestionAnswered) {
@@ -44,9 +89,12 @@ export default function PreCounsellingForm() {
       );
       return;
     }
-    if (currentStep <= totalSteps) {
+    if (currentStep < totalSteps) {
       updateCurrentStep(currentStep + 1);
+      return;
     }
+
+    handleFormSubmit();
   };
 
   const prevStep = () => {
@@ -96,22 +144,26 @@ export default function PreCounsellingForm() {
         </p>
       )}
       {/* Form Navigation Buttons */}
-      <div className="flex space-x-4 justify-center mt-4">
-        <button
-          onClick={prevStep}
-          disabled={!currentStep}
-          className="px-4 py-2 bg-gray-400 text-white rounded disabled:opacity-50"
-        >
-          Previous
-        </button>
-        <button
-          onClick={nextStep}
-          // disabled={disableNextButton}
-          className="px-4 py-2 bg-blue-600 text-white rounded disabled:opacity-50"
-        >
-          {currentStep === totalSteps ? "Submit" : "Next"}
-        </button>
-      </div>
+      {apiStatus === "success" || apiStatus === "error" ? (
+        <></>
+      ) : (
+        <div className="flex space-x-4 justify-center mt-4">
+          <button
+            onClick={prevStep}
+            disabled={!currentStep}
+            className="px-4 py-2 bg-gray-400 text-white rounded disabled:opacity-50"
+          >
+            Previous
+          </button>
+          <button
+            onClick={nextStep}
+            // disabled={disableNextButton}
+            className="px-4 py-2 bg-blue-600 text-white rounded disabled:opacity-50"
+          >
+            {currentStep === totalSteps ? "Submit" : "Next"}
+          </button>
+        </div>
+      )}
     </div>
   );
 }
