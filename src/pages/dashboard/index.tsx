647a71f444f6ab0a15905d57fe1db1ef477d0a74
dvbuiilds <varshneydhairya4@gmail.com>
2025-03-06 00:58:29 +0530

    chore: removed unwanted route ctas & added Alert for pre counselling form.

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,49 +1,20 @@
-import Image from "next/image";
-
-import FreeCounselling from "../../../public/free-counselling.png";
-import ExploreUniversities from "../../../public/explore-universities.png";
 import Link from "next/link";
+import { Alert } from "@app/components/Alert";
+import { useUserSession } from "@app/context/UserSessionContext";
 
 const Dashboard = () => {
+  const { user } = useUserSession();
+
   return (
-    <div className="flex items-center justify-center align-center h-full gap-4 bg-white">
-      <Link href={"/dashboard/counselling"}>
-        <div className="p-6 bg-white shadow rounded-lg flex flex-col items-center content-center gap-2">
-          <Image
-            priority
-            src={FreeCounselling}
-            alt="Avail Free Counselling"
-            height={200}
+    <div className="flex items-start justify-center align-center h-full gap-4 bg-white rounded-md p-1">
+      {!user?.haveFilledPreCounsellingForm ? (
+        <Link href={"/dashboard/pre-counselling-form"} className="w-full">
+          <Alert
+            message="Fill up this brief form and unlock free resources worth $1000."
+            type="info"
           />
-          <div className="flex flex-col items-center">
-            <h3 className="text-lg font-semibold">Free Counselling</h3>
-            <p className="text-gray-600">
-              Schedule your free 30-minute session with expert counselors.
-            </p>
-          </div>
-          <button className="px-4 bg-blue-500 text-white p-2 rounded hover:bg-blue-600 disabled:bg-blue-300 font-bold cursor-pointer">
-            Start Now {">"}
-          </button>
-        </div>
-      </Link>
-      <Link href={"/dashboard/explore-universities"}>
-        <div className="p-6 bg-white shadow rounded-lg flex flex-col items-center content-center gap-2">
-          <Image
-            src={ExploreUniversities}
-            alt="Explore Universities for free"
-            height={200}
-          />
-          <div className="flex flex-col items-center">
-            <h3 className="text-lg font-semibold">Explore Universities</h3>
-            <p className="text-gray-600">
-              Discover top universities worldwide tailored to your preferences.
-            </p>
-          </div>
-          <button className="px-4 bg-blue-500 text-white p-2 rounded hover:bg-blue-600 disabled:bg-blue-300 font-bold cursor-pointer">
-            Explore Now {">"}
-          </button>
-        </div>
-      </Link>
+        </Link>
+      ) : null}
     </div>
   );
 };
