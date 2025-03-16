21726b165dfbd45594428c6da758181b99e8b0c2
dvbuiilds <varshneydhairya4@gmail.com>
2025-03-16 15:55:23 +0530

    chore(get-blog): added query param for pageId.

--- src/pages/api/blogs/get-blog.ts
+++ src/pages/api/blogs/get-blog.ts
@@ -13,27 +13,24 @@ export default async function handler(
   if (req.method !== "GET") {
     return res.status(405).json({
       success: false,
-      error: { message: "HTTP Method not allowed. ", status: 405 },
+      error: { message: "HTTP Method not allowed. " },
     });
   }
 
   try {
-    // pageId from the request body.
-    const body = await JSON.parse(req.body);
-    const pageId = body.pageId;
-    console.log("@@ pageId", pageId);
+    const pageId = req.query.pageId;
     const response = await blogsCollection.findOne({
       pageId,
     });
     if (!response) {
       return res.status(404).json({
         success: false,
-        error: { message: "Page not found.", status: 404 },
+        error: { message: "Page not found." },
       });
     }
     return res.status(200).json({
       success: true,
-      data: response.data,
+      data: response,
     });
   } catch (error) {
     console.error("Error fetching blog data: ", error);
