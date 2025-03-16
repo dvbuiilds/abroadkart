3b58033bae87f89250fd407b0c93cdf1dadd5e2c
dvbuiilds <varshneydhairya4@gmail.com>
2025-03-16 19:04:50 +0530

    chore(blog page): added render section for different page sections.

--- src/pages/blogs/[pageId].tsx
+++ src/pages/blogs/[pageId].tsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+// ICONS
+import { BsDot } from "react-icons/bs";
+
 // TYPES
 import type { GetServerSidePropsContext } from "next";
 import type { ResponseType } from "../../types/api-types";
@@ -10,9 +13,21 @@ import { fetchWithTimeout } from "@app/utils/fetch-utils";
 // CONFIGS
 import { apiEndPoints, apiPath } from "@app/config/api-config";
 
+const PageSectionKeysMap = {
+  h1: "h1",
+  h2: "h2",
+  h3: "h3",
+  h4: "h4",
+  p: "p",
+  img: "img",
+  a: "a",
+} as const;
+
+type PageSectionKeys = keyof typeof PageSectionKeysMap;
+
 interface PageDataType {
-  id: string;
-  data: {}[];
+  pageId: string;
+  data: Record<PageSectionKeys, string>[];
 }
 
 export const getServerSideProps = async (
@@ -38,9 +53,80 @@ export const getServerSideProps = async (
   };
 };
 
-const BlogPage = (props: { pageData: [] }) => {
-  console.log("@@ props", props);
-  return <div>BlogPage</div>;
+const renderSection = (
+  section: Record<PageSectionKeys, string>,
+  index: number
+) => {
+  const key = Object.keys(section)[0];
+  const content = section[key as PageSectionKeys];
+
+  switch (key) {
+    case PageSectionKeysMap.h1: {
+      return (
+        <h1
+          className="text-2xl font-semibold py-3 md:font-bold md:text-3xl"
+          key={`h1-${index}`}
+        >
+          {content}
+        </h1>
+      );
+    }
+    case PageSectionKeysMap.h2: {
+      return (
+        <h2
+          className="text-xl font-medium py-2 md:font-semibold md:text-2xl"
+          key={`h2-${index}`}
+        >
+          {content}
+        </h2>
+      );
+    }
+    case PageSectionKeysMap.h3: {
+      return (
+        <h3 className="" key={`h3-${index}`}>
+          {content}
+        </h3>
+      );
+    }
+    case PageSectionKeysMap.h4: {
+      return (
+        <h4 className="" key={`h4-${index}`}>
+          {content}
+        </h4>
+      );
+    }
+    case PageSectionKeysMap.p: {
+      return (
+        <p className="text-justify" key={`p-${index}`}>
+          {content}
+        </p>
+      );
+    }
+    case PageSectionKeysMap.img: {
+      return <img className="" key={`img-${index}`} src={content} />;
+    }
+    // case PageSectionKeysMap.a: {
+    //   return <
+    // }
+    default: {
+      return <></>;
+    }
+  }
+};
+
+const BlogPage = (props: { pageData: PageDataType }) => {
+  return (
+    <div className="px-2 py-2 md:px-32 lg:px-48">
+      <div className="flex flex-row items-center justify-center ">
+        <a href="/blogs" className="text-decoration-none">
+          Blogs
+        </a>
+        <BsDot size={32} />
+        <p>{"Title of the Page"}</p>
+      </div>
+      {props.pageData.data.map(renderSection)}
+    </div>
+  );
 };
 
 export default BlogPage;
