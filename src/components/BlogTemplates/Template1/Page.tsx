c694a83e54b758663e4e718b3536f008fbc3cd5a
dvbuiilds <varshneydhairya4@gmail.com>
2025-03-18 22:01:09 +0530

    chore: added Page Template for blog with renderSection.

---
+++ src/components/BlogTemplates/Template1/Page.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { BsDot } from "react-icons/bs";
+import { FaUsersLine } from "react-icons/fa6";
+import type { BlogPageData, BlogSectionType } from "./types";
+import { PageSectionKeysMap } from "./config";
+
+const renderSection = (section: BlogSectionType, index: number) => {
+  switch (section.sectionType) {
+    case PageSectionKeysMap.h1: {
+      return (
+        <h1 className="text-2xl font-semibold py-3 md:font-bold md:text-3xl">
+          {section.content}
+        </h1>
+      );
+    }
+    case PageSectionKeysMap.h2: {
+      return (
+        <h2 className="text-xl font-medium py-2 md:font-semibold md:text-2xl">
+          {section.content}
+        </h2>
+      );
+    }
+    case PageSectionKeysMap.h3: {
+      return <h3 className="">{section.content}</h3>;
+    }
+    case PageSectionKeysMap.h4: {
+      return <h4 className="">{section.content}</h4>;
+    }
+    case PageSectionKeysMap.p: {
+      return <p className="text-justify">{section.content}</p>;
+    }
+    case PageSectionKeysMap.img: {
+      return (
+        <div className="flex flex-col items-center justify-center gap-1">
+          <Image
+            src={section.src}
+            alt={section.alt}
+            width={1000}
+            height={600}
+            priority
+          />
+          <p className="text-center">{section.imgCredit}</p>
+        </div>
+      );
+    }
+    case PageSectionKeysMap.a: {
+      return (
+        <Link href={section.href}>{`${section.label}: ${section.cta}`}</Link>
+      );
+    }
+    case PageSectionKeysMap.info: {
+      return (
+        <div className="flex flex-row items-center justify-between">
+          <div className="flex flex-row items-center justify-center gap-3">
+            <FaUsersLine size={42} />
+            <div className="flex flex-col justify-center">
+              <p className="text-sm font-semibold">{section.authorName}</p>
+              <p className="text-xs">{section.authorBio}</p>
+            </div>
+          </div>
+          <p className="italic">{section.datePublished}</p>
+        </div>
+      );
+    }
+    default: {
+      return null;
+    }
+  }
+};
+
+export const Page = (props: { data: BlogPageData }) => {
+  return (
+    <div className="px-2 py-2 md:px-32 lg:px-48">
+      <div className="flex flex-row items-center justify-center ">
+        <Link href="/blogs" className="text-decoration-none">
+          Blogs
+        </Link>
+        <BsDot size={32} />
+        <p>{"Title of the Page"}</p>
+      </div>
+      {props.data.pageData.map((section, index) => (
+        <div key={`section-${section.sectionType}_${index}`} className="py-1">
+          {renderSection(section, index)}
+        </div>
+      ))}
+    </div>
+  );
+};
--- src/pages/blogs/[pageId].tsx
+++ src/pages/blogs/[pageId].tsx
@@ -1,37 +1,18 @@
 import React from "react";
 
-import Link from "next/link";
-import Image from "next/image";
-
-// ICONS
-import { BsDot } from "react-icons/bs";
+// COMPONENTS
+import { Page as BlogPageTemplate } from "@app/components/BlogTemplates/Template1/Page";
 
 // TYPES
 import type { GetServerSidePropsContext } from "next";
 import type { ResponseType } from "../../types/api-types";
+import type { BlogPageData } from "@app/components/BlogTemplates/Template1/types";
 
 // UTILS
 import { fetchWithTimeout } from "@app/utils/fetch-utils";
 
 // CONFIGS
 import { apiEndPoints, apiPath } from "@app/config/api-config";
-
-const PageSectionKeysMap = {
-  h1: "h1",
-  h2: "h2",
-  h3: "h3",
-  h4: "h4",
-  p: "p",
-  img: "img",
-  a: "a",
-} as const;
-
-type PageSectionKeys = keyof typeof PageSectionKeysMap;
-
-interface PageDataType {
-  pageId: string;
-  data: Record<PageSectionKeys, string>[];
-}
 
 export const getServerSideProps = async (
   context: GetServerSidePropsContext
@@ -48,14 +29,14 @@ export const getServerSideProps = async (
     };
   }
 
-  const jsonResponse: ResponseType<PageDataType> = response.data;
-
+  const jsonResponse: ResponseType<BlogPageData> = response.data;
   if (!jsonResponse.success) {
     console.error("pageData not fetched. ", jsonResponse.error);
     return {
       notFound: true,
     };
   }
+
   return {
     props: {
       pageData: jsonResponse.data,
@@ -63,82 +44,8 @@ export const getServerSideProps = async (
   };
 };
 
-const renderSection = (
-  section: Record<PageSectionKeys, string>,
-  index: number
-) => {
-  const key = Object.keys(section)[0];
-  const content = section[key as PageSectionKeys];
-
-  switch (key) {
-    case PageSectionKeysMap.h1: {
-      return (
-        <h1
-          className="text-2xl font-semibold py-3 md:font-bold md:text-3xl"
-          key={`h1-${index}`}
-        >
-          {content}
-        </h1>
-      );
-    }
-    case PageSectionKeysMap.h2: {
-      return (
-        <h2
-          className="text-xl font-medium py-2 md:font-semibold md:text-2xl"
-          key={`h2-${index}`}
-        >
-          {content}
-        </h2>
-      );
-    }
-    case PageSectionKeysMap.h3: {
-      return (
-        <h3 className="" key={`h3-${index}`}>
-          {content}
-        </h3>
-      );
-    }
-    case PageSectionKeysMap.h4: {
-      return (
-        <h4 className="" key={`h4-${index}`}>
-          {content}
-        </h4>
-      );
-    }
-    case PageSectionKeysMap.p: {
-      return (
-        <p className="text-justify" key={`p-${index}`}>
-          {content}
-        </p>
-      );
-    }
-    case PageSectionKeysMap.img: {
-      return (
-        <Image className="" key={`img-${index}`} src={content} alt="Alt Text" />
-      );
-    }
-    // case PageSectionKeysMap.a: {
-    //   return <
-    // }
-    default: {
-      return <></>;
-    }
-  }
-};
-
-const BlogPage = (props: { pageData: PageDataType }) => {
-  return (
-    <div className="px-2 py-2 md:px-32 lg:px-48">
-      <div className="flex flex-row items-center justify-center ">
-        <Link href="/blogs" className="text-decoration-none">
-          Blogs
-        </Link>
-        <BsDot size={32} />
-        <p>{"Title of the Page"}</p>
-      </div>
-      {props.pageData.data.map(renderSection)}
-    </div>
-  );
+const BlogPage = (props: { pageData: BlogPageData }) => {
+  return <BlogPageTemplate data={props.pageData} />;
 };
 
 export default BlogPage;
