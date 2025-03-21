b7ed1644f5ad4c8153506296c68c341d7d7a2099
dvbuiilds <varshneydhairya4@gmail.com>
2025-03-21 19:28:58 +0530

    chore: added api for updating data of a blog.

---
+++ src/pages/api/blogs/update-blog.ts
@@ -0,0 +1,59 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+// TYPES
+import { BlogPageData } from "@app/components/BlogTemplates/Template1/types";
+
+// UTILS
+import mongoDBClient from "../../../server/db/mongodb";
+
+const db = mongoDBClient.db();
+const blogsCollection = db.collection("blogs");
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "PUT") {
+    return res.status(405).json({
+      success: false,
+      error: { message: "HTTP Method not allowed. " },
+    });
+  }
+
+  try {
+    const data: BlogPageData =
+      typeof req.body === "string" ? await JSON.parse(req.body) : req.body;
+
+    // Validation: Check if required fields are present
+    if (!data.pageId || !data.title || !data.category || !data.pageData) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Missing required fields" });
+    }
+
+    // Find and update the blog entry in the database
+    const updatedBlog = await blogsCollection.findOneAndUpdate(
+      { pageId: data.pageId }, // Find blog by pageId
+      { $set: { ...data } }, // Update fields
+      { returnDocument: "after" }
+    );
+
+    if (!updatedBlog) {
+      return res.status(404).json({
+        success: false,
+        error: { message: "Blog not found!", status: 404 },
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      data: updatedBlog,
+    });
+  } catch (error) {
+    console.error("Error updating the blog: ", error);
+    return res.status(500).json({
+      success: false,
+      message: "Error updating blog.",
+    });
+  }
+}
