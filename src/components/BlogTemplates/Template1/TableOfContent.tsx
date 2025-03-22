d009899f685f0cb981402cfb49500216f2aad1d5
dvbuiilds <varshneydhairya4@gmail.com>
2025-03-22 13:36:08 +0530

    chore: added handleScroll for run time navigation to section on page.

--- src/components/BlogTemplates/Template1/TableOfContent.tsx
+++ src/components/BlogTemplates/Template1/TableOfContent.tsx
@@ -2,6 +2,14 @@ import React from "react";
 import type { TableOfContentsSection } from "./types";
 
 export const TableOfContents = (props: { data: TableOfContentsSection }) => {
+  // I am writing this scroll handler seperately because the default onClick event handler is not binding properly causing no effect when the list items are clicked/pressed.
+  const handleScroll = (id: string) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  };
+
   return (
     <div className="px-3">
       <h2 className="text-xl font-medium py-2 md:font-semibold md:text-2xl">
@@ -11,7 +19,12 @@ export const TableOfContents = (props: { data: TableOfContentsSection }) => {
         <ol className="list-decimal list-inside">
           {props.data.content.map((node, index) => (
             <li key={`node-${index}`}>
-              <a href={node.id} className="" title={node.label}>
+              <a
+                href={node.id}
+                className=""
+                title={node.label}
+                onClick={() => handleScroll(node.id)}
+              >
                 {node.label}
               </a>
               <ol className="list-decimal list-inside">
