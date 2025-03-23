1831966d21cdd65440f91c0f1454d07d16fa0ea8
dvbuiilds <varshneydhairya4@gmail.com>
2025-03-23 16:34:57 +0530

    chore(BlogInfo): added UI component for Blog Info.

---
+++ src/components/BlogTemplates/Template1/BlogInfo.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import type { InfoSection } from "./types";
+
+export const BlogInfo = (props: { data: InfoSection }) => {
+  return (
+    <div className="flex flex-row items-center justify-between">
+      <div className="text-gray-600">
+        <p>
+          Written by:
+          <strong>{` ${props.data.authorName} -`}</strong>
+          <span>{props.data.authorBio}</span>
+        </p>
+        <p>
+          {props.data.publishedDate === props.data.lastModifiedDate ? (
+            <span className="text-sm italic">{`Published Date: ${props.data.publishedDate}`}</span>
+          ) : (
+            <span className="text-sm italic">{`Last Modified Date: ${props.data.lastModifiedDate}`}</span>
+          )}
+        </p>
+      </div>
+    </div>
+  );
+};
