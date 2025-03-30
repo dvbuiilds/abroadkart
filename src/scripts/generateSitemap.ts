25862aaa069b1e5afeae80c549ac8e4c1d390a70
dvbuiilds <varshneydhairya4@gmail.com>
2025-03-30 22:56:12 +0530

    chore(generate sitemap): changed the structure of url block in sitemap.

--- src/scripts/generateSitemap.ts
+++ src/scripts/generateSitemap.ts
@@ -1,23 +1,94 @@
 import fs from "fs";
 import path from "path";
 
+interface XMLURLBlock {
+  id: string;
+  lastMod: string;
+  changeFreq: "daily" | "weekly" | "monthly" | "yearly";
+  priority: number;
+}
+
 const SITE_URL = "https://abroadkart.com";
 
+const staticPages: XMLURLBlock[] = [
+  { id: "", lastMod: "2025-03-30", changeFreq: "weekly", priority: 1.0 },
+  {
+    id: "about",
+    lastMod: "2025-03-30",
+    changeFreq: "monthly",
+    priority: 0.9,
+  },
+  {
+    id: "contact",
+    lastMod: "2025-03-30",
+    changeFreq: "monthly",
+    priority: 0.7,
+  },
+  {
+    id: "privacy-policy",
+    lastMod: "2025-03-30",
+    changeFreq: "yearly",
+    priority: 0.3,
+  },
+  {
+    id: "terms",
+    lastMod: "2025-03-30",
+    changeFreq: "yearly",
+    priority: 0.3,
+  },
+];
+
 // Function to fetch blog slugs from DB
-const fetchBlogSlugs = async (): Promise<string[]> => {
-  return ["first-blog", "second-blog", "third-blog"]; // Replace with DB query
+const fetchBlogSlugs = async (): Promise<XMLURLBlock[]> => {
+  return [
+    {
+      id: "first-blog",
+      lastMod: "2025-03-30",
+      changeFreq: "yearly",
+      priority: 1.0,
+    },
+    {
+      id: "second-blog",
+      lastMod: "2025-03-30",
+      changeFreq: "yearly",
+      priority: 1.0,
+    },
+    {
+      id: "third-blog",
+      lastMod: "2025-03-30",
+      changeFreq: "yearly",
+      priority: 1.0,
+    },
+  ]; // Replace with DB query
 };
 
 // Generate Sitemap
 export const generateSitemap = async () => {
   try {
     const blogSlugs = await fetchBlogSlugs();
-    const staticPages = ["", "about", "contact", "services"];
 
     const urls = [
-      ...staticPages.map((page) => `<url><loc>${SITE_URL}/${page}</loc></url>`),
+      ...staticPages.map(
+        (page) => `<url>
+        <loc>${SITE_URL}/${page.id}</loc>
+        <lastmod>${page.lastMod}</lastmod>
+        <changefreq>${page.changeFreq}</changefreq>
+        <priority>${page.priority}</priority>
+      </url>`
+      ),
+      `<url>
+        <loc>${SITE_URL}/blogs</loc>
+        <lastmod>2025-03-30</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.8</priority>
+      </url>`,
       ...blogSlugs.map(
-        (slug) => `<url><loc>${SITE_URL}/blog/${slug}</loc></url>`
+        (blog) => `<url>
+        <loc>${SITE_URL}/blogs/${blog.id}</loc>
+        <lastmod>${blog.lastMod}</lastmod>
+        <changefreq>${blog.changeFreq}</changefreq>
+        <priority>1.0</priority>
+        </url>`
       ),
     ];
 
