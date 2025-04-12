3fb3b4567c28796172ab4eb0ff8e434d174e7afd
dvbuiilds <varshneydhairya4@gmail.com>
2025-04-12 13:24:09 +0530

    chore(form-types): added new types for accomodating single and multiple select options.

--- src/types/form-types.ts
+++ src/types/form-types.ts
@@ -1,11 +1,40 @@
-export interface QuestionnaireItemInfo {
+import { QuestionTypeMap } from "@app/config/form-questions-config";
+
+type QuestionCommonAttributes = {
+  required?: boolean;
+  validationRegex?: string;
+};
+
+export interface TextQuestionObject extends QuestionCommonAttributes {
+  type: typeof QuestionTypeMap.TEXT;
   question: string;
   answer: string;
 }
 
-export interface QuestionnaireItem {
-  name: string;
-  info: QuestionnaireItemInfo[];
+export interface SelectQuestionObject extends QuestionCommonAttributes {
+  type: typeof QuestionTypeMap.SELECT;
+  question: string;
+  options: string[];
+  answer: string;
 }
 
-export type Questionnaire = QuestionnaireItem[];
+export interface MultiSelectQuestionObject extends QuestionCommonAttributes {
+  type: typeof QuestionTypeMap.MULTISELECT;
+  question: string;
+  options: string[];
+  answer: string[];
+}
+
+export type QuestionObject =
+  | TextQuestionObject
+  | SelectQuestionObject
+  | MultiSelectQuestionObject;
+
+export type QuestionObjects = QuestionObject[];
+
+export interface QuestionsSet {
+  name: string;
+  questions: QuestionObjects;
+}
+
+export type QuestionsSets = QuestionsSet[];
