20eeeca6d106bea4943747f2243f703854742a28
Dhairya Varshney <varshneydhairya4@gmail.com>
2026-04-26 16:09:01 +0530

    fix: enable forcePathStyle for S3 compatibility
    - Updated the Keystone configuration to set forcePathStyle to true, ensuring compatibility with S3-compatible storage solutions.
    - This change enhances the handling of bucket paths in the application, improving overall functionality.

--- keystone/keystone.ts
+++ keystone/keystone.ts
@@ -69,6 +69,7 @@ export default config({
       bucketName: process.env.R2_BUCKET_NAME!,
       region: "auto",
       endpoint: process.env.R2_ENDPOINT,
+      forcePathStyle: true,
       accessKeyId: process.env.R2_ACCESS_KEY_ID!,
       secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!,
       signed: { expiry: 3600 },
